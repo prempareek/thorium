c93d0c19f2fbbb07912e4835fbb25304f5a7b84d
prempareek <prempareekech@gmail.com>
2022-02-21 17:09:04 +0530

    my_first_assignment

---
+++ src/routes/logger.js
@@ -0,0 +1,9 @@
+let url = 'https://www.google.com'
+
+function printMyMessage(message) {
+    console.log(message)
+}
+
+
+module.exports.endpoint = url
+module.exports.printMessage = printMyMessage
